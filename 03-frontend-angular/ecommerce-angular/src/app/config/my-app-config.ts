a7763427e13ba43dc31402d1fc8897ae0788fa44
sumitkumar <sumitkumar1202@gmail.com>
2023-12-19 13:19:00 +0530

    minor refactor

--- 03-frontend-angular/ecommerce-angular/src/app/config/my-app-config.ts
+++ 03-frontend-angular/ecommerce-angular/src/app/config/my-app-config.ts
@@ -1,7 +1,7 @@
 export default {
   oidc: {
-    clientId: '0oadyycp3zfH2lIpC5d7',
-    issuer: 'https://dev-16949299.okta.com/oauth2/default',
+    clientId: '{{clientId}}',
+    issuer: '{{issuer}}',
     redirectUri: 'http://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email'],
   },
